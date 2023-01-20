9262e91ebfd93315e2e2ebdc1289472ddddfdb89
rafroes1 <rafafroes@outlook.com>
2023-01-20 09:36:53 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Rafael Carvalho on 20/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
